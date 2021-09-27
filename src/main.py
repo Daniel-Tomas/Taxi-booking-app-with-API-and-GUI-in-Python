42fae3a5218bef10002cd9ca60d31e257e313816
juan-vmarin <62159985+juan-vmarin@users.noreply.github.com>
2021-09-27 17:49:46 +0200

    Modofied admin GUI, and created the list and tooltips

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
+# import src.admin_ui
+
+from src import admin_ui
+from src import login_ui
 from login_ui import Ui_login_dialog
-from admin_ui import *
+from admin_ui import Ui_Dialog
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -12,14 +16,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         # Button
-        self.button_login = QtWidgets.QAction("Admin")
-        self.button_login.triggered(self.to_admin)
+        self.ui.login_button.clicked.connect(self.to_admin)
 
     def to_admin(self):
-        self.admin_window = admin_ui.QtWidgets.QMainWindow()
-        admin_ui = admin_ui.Ui_Admin_Window()
-        admin_ui.setupUi(self.admin_window)
+        self.admin_window = admin_ui.QtWidgets.QDialog()
+        ad_ui = admin_ui.Ui_Dialog()
+        ad_ui.setupUi(self.admin_window)
         self.admin_window.show()
+
+        # self.ui2 = Ui_Dialog()
+        # self.ui2.setupUi(self)
+
+    def to_users(self):
+        pass
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
